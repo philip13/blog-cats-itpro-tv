c2da411ef81b06722d901b666c1a996b5676d7f3
Felipe de Jesús Cuevas Castillo <felipedejesuscuevascastillo@MacBook-Pro-de-Felipe.local>
2022-06-15 12:24:17 -0500

    app/models/post.rb add validations

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,3 +1,14 @@
 class Post < ApplicationRecord
+ validates :title, presence: true, uniqueness: true
+
+ validates :body, presence: true
+ validate :cant_say_balderdash
+
+
+ def cant_say_balderdash
+  if body =~ /balderdash/
+    error.add :body, "Can't say that word!" # :base base is used if want a global error
+  end
+ end
 
 end
