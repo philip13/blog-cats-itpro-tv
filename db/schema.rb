ada8ed417cce9c49d1cde9c3af0a9a9a1c20d5ef
Felipe Cuevas <felipe.cuevas@distillery.com>
2022-06-17 16:35:34 -0500

    Adding picture to post

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_06_16_203740) do
+ActiveRecord::Schema.define(version: 2022_06_17_190839) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,10 @@ ActiveRecord::Schema.define(version: 2022_06_16_203740) do
     t.string "title"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.bigint "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
