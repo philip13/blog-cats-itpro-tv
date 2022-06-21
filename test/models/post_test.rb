ac8054809265119e04579f3590bece39a45e03dc
Felipe Cuevas <felipe.cuevas@distillery.com>
2022-06-21 13:42:22 -0500

    test/models/post_test.rb include some test model validations

--- test/models/post_test.rb
+++ test/models/post_test.rb
@@ -1,7 +1,15 @@
 require 'test_helper'
 
 class PostTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "can't say balderdash" do
+    @post = Post.new( title: "anything", body: "balderdash")
+
+    assert_not @post.valid? 
+  end
+
+  test "Create new valid post" do
+    @post = Post.new( title: "anything 2", body: "valid")
+
+    assert @post.valid? 
+  end
 end
