be829797bc1da44805eaddee5757cedc89c5a67b
Felipe Cuevas <felipe.cuevas@distillery.com>
2022-06-22 19:50:35 -0500

    Install tailwindcss@2.2.17

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -15,4 +15,15 @@
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
 
-console.log('Hello World from Webpacker')
+
+
+// require("@rails/ujs").start();
+// require("turbolinks").start();
+// require("@rails/activestorage").start();
+// require("channels");
+require("stylesheets/application.scss");
+
+
+// $(document).on('turbolinks:load', function() {
+// 	console.log('Hello World from Webpacker');
+// });
