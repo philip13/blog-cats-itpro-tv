12a9ea4af2edcf149506debec58933c1426de2ec
Felipe Cuevas <felipe.cuevas@distillery.com>
2022-07-01 12:40:42 -0500

    app/views/shared/_navbar.html.erb app/helpers/application_helper.rb include about us link and update active link

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def active_link pats
+    pats.include?( request.fullpath ) ? "text-red-300 border-b-4 border-red-300" : "text-gray-500"
+  end
+
+  def active_link_mb pats
+    pats.include?( request.fullpath ) ? "text-white bg-red-300" : ""
+  end
 end
